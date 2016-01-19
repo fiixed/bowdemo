58c6800fa37163dc4bd9b7c4dd800cc4d44383e3
Andrew Bell <fiixed@gmail.com>
2016-01-19 14:13:39 -0600

    CSS styling

--- www/js/services.js
+++ www/js/services.js
@@ -1,27 +1,27 @@
 angular.module('starter.services', [])
 
 .service('LoginService', function($q) {
-    return {
-        loginUser: function(name, pw) {
-            var deferred = $q.defer();
-            var promise = deferred.promise;
+  return {
+    loginUser: function(name, pw) {
+      var deferred = $q.defer();
+      var promise = deferred.promise;
 
-            if (name == 'user' && pw == 'user') {
-                deferred.resolve('Welcome ' + name + '!');
-            } else {
-                deferred.reject('Wrong credentials.');
-            }
-            promise.success = function(fn) {
-                promise.then(fn);
-                return promise;
-            }
-            promise.error = function(fn) {
-                promise.then(null, fn);
-                return promise;
-            }
-            return promise;
-        }
+      if (name == 'user' && pw == 'user') {
+        deferred.resolve('Welcome ' + name + '!');
+      } else {
+        deferred.reject('Wrong credentials.');
+      }
+      promise.success = function(fn) {
+        promise.then(fn);
+        return promise;
+      }
+      promise.error = function(fn) {
+        promise.then(null, fn);
+        return promise;
+      }
+      return promise;
     }
+  }
 })
 .factory('Camera', ['$q', function($q) {
 
@@ -41,13 +41,27 @@ angular.module('starter.services', [])
   }
 }])
 .factory('jillreacher', function(){
-  var jillreacher = {
+  var jillreacher = [{
     name: "Jill Reacher",
     department: "Digital Business Services",
     title: "UX Designer",
     description: "I'm working on a new format for UI spec for Mattel.",
     date: "Jun 7 7:21PM",
     imageURI: ""
-  };
+  }, {
+    name: "Rick Helmick",
+    department: "Digital Business Services",
+    title: "UX Designer",
+    description: "I'm working on a new format for UI spec for Mattel.",
+    date: "Jun 6 5:10PM",
+    imageURI: ""
+  },{
+    name: "Jack Reacher",
+    department: "Digital Business Services",
+    title: "UX Designer",
+    description: "I'm working on a new format for UI spec for Mattel.",
+    date: "Jun 3 4:20PM",
+    imageURI: ""
+  }];
   return jillreacher;
 });
